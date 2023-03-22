9a1af7e582e8af81753bed52b3a3176dbb5bd1de
anaijg <ana.jimenezgomez@educa.madrid.org>
2023-03-22 19:17:00 +0100

    Punto

---
+++ ClasesAvanzadas/src/figuras/Main.java
@@ -0,0 +1,11 @@
+package figuras;
+
+public class Main {
+    public static void main(String[] args) {
+        // Puntos:
+        Punto p1 = new Punto(1, 0);
+        Punto p2 = new Punto(0, 1);
+        Punto p3 = new Punto(2, 2);
+        Punto p4 = new Punto(2, 3);
+    }
+}
